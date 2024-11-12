36664d843e4bdace23fa50a437d3a8bde13d27b4
Rafaelmalaquias92 <rafaelmalaquias92@gmail.com>
2024-11-12 17:26:54 -0300

    bancodedados

--- comandos-sql.md
+++ comandos-sql.md
@@ -16,3 +16,59 @@ VALUES ('Rafaelmalaquiassantos','rafaelmalaquiassantos@gmail','ATECUBANOS1234','
 
 INSERT INTO usuarios (nome, email, senha, tipo)
 VALUES ('Goku','kakaroto@gmail','BULMAS4F4D4','editor');
+
+### Inserindo noticias
+
+INSERT INTO noticias (titulo, texto, resumo, imagem, usuario_id)
+VALUES (
+    'Meu pai ganhou na mega-sena',
+    'To rico, tchau pra vcs',
+    'Jogou e ganhou',
+    'premio.jpg',
+    1
+);
+
+INSERT INTO noticias (titulo, texto, resumo, imagem, usuario_id)
+VALUES (
+    'Eu vou li pegar',
+    'Tu vai ver só',
+    'pegou',
+    'surra.jpg',
+    2
+);
+
+INSERT INTO noticias (titulo, texto, resumo, imagem, usuario_id)
+VALUES (
+    'tira, tira que eu vou...',
+    'Problemas estomacais',
+    '💩',
+    'tira.jpg',
+    3
+);
+
+INSERT INTO noticias (titulo, texto, resumo, imagem, usuario_id)
+VALUES (
+    'Normal',
+    'lavar a louça que é bom nada ',
+    '🏳‍🌈',
+    'lavar.jpg',
+    4
+);
+
+## Leitura de dados da tabela "noticias"
+
+SELECT data, titulo FROM noticias;
+
+## Leitura de dados da tabela "usuarios"
+
+SELECT nome, email, tipo FROM usuarios;  
+SELECT nome, email, tipo FROM usuarios WHERE tipo = 'editor';  
+
+## Atualização de dados dos usuários
+
+UPDATE usuarios SET email = 'rafael@gmail.com'
+WHERE id = 1;
+
+## Excluindo dados da tabela "noticias"
+
+DELETE FROM noticias WHERE id = 1;
---
+++ microblog_rafael.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12/11/2024 às 21:19
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `microblog_rafael`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id` int(11) NOT NULL,
+  `data` datetime NOT NULL DEFAULT current_timestamp(),
+  `titulo` varchar(150) NOT NULL,
+  `texto` text NOT NULL,
+  `resumo` text NOT NULL,
+  `imagem` varchar(45) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `noticias`
+--
+
+INSERT INTO `noticias` (`id`, `data`, `titulo`, `texto`, `resumo`, `imagem`, `usuario_id`) VALUES
+(2, '2024-11-12 16:38:32', 'Eu vou li pegar', 'Tu vai ver só', 'pegou', 'surra.jpg', 2),
+(4, '2024-11-12 16:42:09', 'tira, tira que eu vou...', 'Problemas estomacais', '💩', 'tira.jpg', 3),
+(5, '2024-11-12 16:46:37', 'Normal', 'lavar a louça que é bom nada ', '🏳‍🌈', 'lavar.jpg', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` enum('admin','editor') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
+(1, 'Rafael', 'rafael@gmail.com', 'ATECUBANOS', 'admin'),
+(2, 'Rafaelmalaquias', 'rafaelmalaquias@gmail', 'ATECUBANOS123', 'editor'),
+(3, 'Rafaelmalaquiassantos', 'rafaelmalaquiassantos@gmail', 'ATECUBANOS1234', 'editor'),
+(4, 'Goku', 'kakaroto@gmail', 'BULMAS4F4D4', 'editor');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_noticias_usuarios` (`usuario_id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `noticias`
+--
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `fk_noticias_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
