1613f7e89033f7415428b53480521b3469e6cf58
Rafaelmalaquias92 <rafaelmalaquias92@gmail.com>
2024-11-14 15:27:03 -0300

    Create funcoesusuarios.php

---
+++ includes/funcoesusuarios.php
@@ -0,0 +1,12 @@
+<?php
+require "conecta.php";
+
+// Função para inserir novos usuarios
+function inserirUsuario($conexao, $nome, $email, $senha, $tipo){
+// Montando o comando SQL para fazer o INSERT dos dados
+$sql = "INSERT INTO usuarios (nome, email, senha, tipo)
+VALUES('$nome', '$email', '$senha', '$tipo')";
+} 
+
+// Executando o comando no banco via php
+mysqli_query($conexao, $sql);
